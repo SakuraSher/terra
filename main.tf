4595cbff3152304287f1f09239da17f6cd882f6c
SakuraSher <sakurasherlocked@gmail.com>
2023-10-28 11:31:47 +0530

    chap 2

--- main.tf
+++ main.tf
@@ -6,7 +6,24 @@ provider "aws" {
 resource "aws_instance" "example"{
 ami = "ami-0ccea833bf267252a"
 instance_type = "t2.micro"
+vpc_security_group_ids =[aws_security_group.instance]
 tags ={
 	Name="terraform-example"
+	}
+user_data = <<-EOF
+		  #!/bin/bash
+		  echo "Hello World" > index.html
+		  nohup busybox httpd -f -p 8080 &
+		  EOF
+user_data_replace_on_change = true
+}
+
+resource aws_security_group instance{
+	name ="terraform-example-instance"
+	ingress{
+	from_port = 8080
+	to_port =8080
+	protocol="tcp"
+	cidr_blocks =["0.0.0.0/0"]
 	}
 }
