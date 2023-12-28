5e5676e77b865f64b7bd787d5868b30d91a33171
SakuraSher <sakurasherlocked@gmail.com>
2023-12-28 22:12:37 +0530

    app on ec2 with rds

--- Chap8/Example/asg/main.tf
+++ Chap8/Example/asg/main.tf
@@ -5,5 +5,15 @@ provider "aws"{
 
 module "asg"{
   source = "../../modules/cluster/asg"
-  
+  cluster_name = "yerobun"
+  instance_type= "t2.micro"
+  min_size=1
+  max_size=2
+  enable_autoscaling=true
+  subnet_ids=
+  ami=
+  custom_tags=
+  target_group_arns=
+  health_check_type="EC2"
+  user_data =""
 }
