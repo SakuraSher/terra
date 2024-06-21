98e3f1be5bc337d41d01a8be9cb22fcefd974f0c
SakuraSher <sakurasherlocked@gmail.com>
2024-06-21 17:19:09 +0530

    add terra files

---
+++ azure/proj2/terraform.tfvars
@@ -0,0 +1,2 @@
+resource_group_name = "GoApp"
+ENV = "test"
---
+++ azure/proj2/variables.tf
@@ -0,0 +1,13 @@
+variable "location" {
+    default = "Southeast Asia"
+  
+}
+
+variable "resource_group_name" {
+
+  default = "app"
+}
+
+variable "ENV" {
+  default = "DEV"
+}
