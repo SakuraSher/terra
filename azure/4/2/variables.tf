95755748624811c5118afa09c5de88902f62d590
SakuraSher <sakurasherlocked@gmail.com>
2024-07-11 23:29:56 +0530

    add

---
+++ azure/4/2/variables.tf
@@ -0,0 +1,31 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Resource group name"
+}
+
+variable "location" {
+  type        = string
+  description = "Location of Azure reource"
+  default     = "westeurope"
+}
+
+variable "service_plan_name" {
+  type        = string
+  description = "Service plan name"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment Name"
+}
+
+variable "web_apps" {
+  type = map(object({
+    name                = string
+    location            = optional(string, "westeurope")
+    serverdatabase_name = string
+  }))
+
+}
+
+
